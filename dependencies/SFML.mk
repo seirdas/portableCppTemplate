78dca130bbe49adf200cbc7a78c57143f9e8fbc5
seirdas <xergio.e@hotmail.com>
2025-03-17 14:27:27 +0100

    added static config in sfml makefile

--- dependencies/SFML.mk
+++ dependencies/SFML.mk
@@ -6,13 +6,19 @@ BUILD_TYPE 					?= release	# if not defined, default is release
 SFML_INCLUDE 				:= -I$(wildcard dependencies/SFML*/SFML*/include)
 
 ######### Libs (linker)
-SFML_LIBS				:= sfml-system sfml-window sfml-graphics sfml-audio sfml-network
+SFML_LIBS					:= sfml-system sfml-window sfml-graphics sfml-audio sfml-network
+
+ifeq ($(findstring static, $(LIBS)), static)
+	SFML_LIBS				:= $(foreach lib,$(SFML_LIBS),$(lib)-s)
+	SFML_LINKS				:= -DSFML_STATIC
+endif
 ifeq ($(findstring debug, $(BUILD_TYPE)), debug)
 	SFML_LIBS				:= $(foreach lib,$(SFML_LIBS),$(lib)-d)
 endif
 
 SFML_LIB_PATH				:= $(wildcard dependencies/SFML*/SFML*/lib)
-SFML_LDFLAGS 				:= -L"$(SFML_LIB_PATH)" $(foreach lib,$(SFML_LIBS),-l$(lib))
+
+SFML_LINKS					+= -L"$(SFML_LIB_PATH)" $(foreach lib,$(SFML_LIBS),-l$(lib))
 
 ######### dlls
 SFML_BIN					:= $(wildcard dependencies/SFML*/SFML*/bin)
@@ -30,7 +36,9 @@ SFML_DLLS 					:= $(notdir $(SFML_DLLS_PATH))
 
 ################################
 
-# use:
+# use in main makefile:
 # $(SFML_INCLUDE) 		- Include path
-# $(SFML_LDFLAGS) 		- Libs to link
-# $(SFML_DLLS)			- dlls to copy
+# $(SFML_LINKS_DYNAMIC) - dynamic Libs to link (ddls)
+# $(SFML_LINKS_STATIC) 	- static Libs to link
+# $(SFML_DLLS)			- dlls to copy to bin folder
+# $(SFML_BIN)			- path to dlls
